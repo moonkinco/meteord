bd2ff9d054423f9838a1a60f3b66814d6a578565
Robin Van Cauter <robin.van.cauter@gmail.com>
2016-06-05 17:45:02 +0200

    Update install_base.sh
    change imagemagick to graphicsmagick

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y libc6 curl bzip2 build-essential python git imagemagick
+apt-get install -y libc6 curl bzip2 build-essential python git graphicsmagick
